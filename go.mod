b4c48212c0d92e58ae743262b0a288d6287e5866
Padilla <padilla@Padillas-MacBook-Pro.local>
2020-10-19 22:06:31 -0500

    added content to each file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/padilladan/NHL-API
+
+go 1.15
--- main.go
+++ main.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"io"
+	"log"
+	"os"
+	"time"
+
+	"github.com/padilladan/NHL-API/nhlApi"
+)
+
+func main() {
+	// help benchmark the request time
+	start := time.Now()
+
+	rosterFile, err := os.OpenFile("rosters.txt", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		log.Fatalf("error opening the file rosters.txt: %v", err)
+	}
+	defer rosterFile.Close()
+
+	wrt := io.MultiWriter(os.Stdout, rosterFile)
+
+	log.SetOutput(wrt)
+
+	teams, err := nhlApi.GetAllTeams()
+	if err != nil {
+		log.Fatalf("error while getting all teams: %v", err)
+	}
+
+	for _, team := range teams {
+		log.Println("-----------------------")
+		log.Println("Name: %v", team.Name)
+		log.Println("-----------------------")
+	}
+
+	log.Printf("took %v", time.Now().Sub(start).String())
+}
